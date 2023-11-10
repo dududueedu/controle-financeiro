c6393250bd0bb73f56b6d6e8b50ff8bdef5a3442
Eduardo Gomes <eduardooffside@gmail.com>
2023-11-10 09:14:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import ESLogo from './data/icons/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="http://eaportfolio.carrd.co" target="_blank">
+          <img src={ESLogo} className="logo" alt="ES logo" />
+        </a>
+      </div>
+      <h1>ES Acessórios</h1>
+    </>
+  )
+}
+
+export default App
