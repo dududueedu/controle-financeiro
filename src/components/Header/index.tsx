269e193e6827487df7bc8ae28415ec7660b6ead4
Eduardo Gomes <eduardooffside@gmail.com>
2023-11-12 16:44:36 -0300

    update position logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 //import ESLogo from "../../data/icons/logo.png";
 import * as S from "./style";
+import ESLogo from "../../data/icons/logo.png";
 
 const Header = () => {
   return (
 
         <S.ContainerStyle>
             <S.HStyle>
-                <S.TStyle> ES Acessórios </S.TStyle>
+                <S.HLogo>
+                  <S.A href="http://eaportfolio.carrd.co" target="_blank">
+                    <S.HImg src={ESLogo} alt="ES logo" />
+                  </S.A>
+                </S.HLogo>  
+                <S.TitleStyle> ES Acessórios </S.TitleStyle>
             </S.HStyle>
         </S.ContainerStyle>
 
--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -1,14 +1,32 @@
 import styled from 'styled-components'
 
 export const ContainerStyle = styled.div`
-    height: 150px;
-    text-align: center;
+    display: flex;   
+    justify-content: center;
+    height: 200px;
     background: teal;
+    text-align: center;
 `;
 
-export const HStyle = styled.h1``;
+export const HStyle = styled.div`
+    display: flex;   
+    align-items: center;
+`;
 
-export const TStyle = styled.div`
-    padding-top: 20px;
+export const HLogo = styled.div`
+    margin-top: -20%;
+    margin-right: 20px;
+`
+
+export const TitleStyle = styled.h1`
+    margin-top: -20%;
     color: #fff;
 `;
+
+export const HImg = styled.img`
+    height: 70px;
+    wight: 70px;
+    border-radius: 100%;
+`
+
+export const A = styled.a``
